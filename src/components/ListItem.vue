f9f99e7ceabf0bc75f982e261c68033ff05c9678
boloko <yurifbeck@gmail.com>
2021-09-11 16:32:23 -0300

    fixed links

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,124 +1,9 @@
 <template>
   <article class="justify-center items-center">
-    <!-- Create By Joker Banny -->
-    <!-- <div class="p-3 bg-gray-100 flex justify-center items-center">
-      <div
-        class="
-          lg:max-w-lg
-          container
-          bg-white
-          rounded-xl
-          shadow-lg
-          transform
-          transition
-          duration-500
-          hover:scale-105
-          hover:shadow-2xl
-        "
-      >
-        <div>
-          <h1
-            class="
-              text-2xl
-              mt-2
-              ml-4
-              font-bold
-              text-red-500
-              cursor-pointer
-              hover:text-gray-900
-              transition
-              duration-100
-              uppercase
-            "
-          >
-            {{ item.title }}
-          </h1>
-          <div class="flex space-x-2 mt-2 ml-3">
-            <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-7 w-7 text-yellow-400"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="black"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                />
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-              </svg>
-            </span>
-            <h3 class="text-lg text-gray-600 font-semibold mb-2">
-              {{ item.cidade }}
-            </h3>
-          </div>
-        </div>
-        <g-image
-          class="w-full cursor-pointer"
-          :src="item.preview_image"
-          alt=""
-        />
-        <div class="flex p-4 justify-between">
-          <div class="flex items-center space-x-2">
-            <h2 class="text-gray-800 font-bold cursor-pointer">Nota</h2>
-          </div>
-          <div class="flex space-x-2">
-            <div class="flex space-x-1 items-center">
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-7 w-7 text-gray-600 cursor-pointer"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                  />
-                </svg>
-              </span>
-            </div>
-            <div class="flex space-x-1 items-center">
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="
-                    h-7
-                    w-7
-                    text-red-500
-                    hover:text-red-400
-                    transition
-                    duration-100
-                    cursor-pointer
-                  "
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </span>
-              <span>{{ item.date }}</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> -->
-
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.11.2/css/all.css"
+    />
     <div
       class="
         xl:max-w-3xl
@@ -158,6 +43,14 @@
       </div>
       <h1 class="mt-4 text-gray-800 text-3xl font-bold cursor-pointer">
         {{ item.title }}
+        <ul class="flex">
+          <li v-for="(n, index) in 4" :key="index">
+            <i
+              :class="[index < +item.nota[0] ? 'fa' : 'far']"
+              class="fa-star fa-sm text-yellow-500 mr-1"
+            ></i>
+          </li>
+        </ul>
       </h1>
       <div class="my-4">
         <div class="flex space-x-1 items-center">
@@ -205,40 +98,25 @@
           <p>{{ item.date }}</p>
         </div>
 
-        <div class="flex space-x-1 items-center">
-          <span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6 text-indigo-600 mb-1.5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
-              />
-            </svg>
-          </span>
-          <p>{{ item.nota }}</p>
-        </div>
-        <button
-          class="
-            mt-4
-            text-xl
-            font-bold
-            w-full
-            text-white
-            bg-red-600
-            py-2
-            rounded-xl
-            shadow-lg
-          "
-        >
-          Ler comentários
-        </button>
+        <div class="flex space-x-1 items-center"></div>
+        <div></div>
+        <g-link :to="item.path">
+          <button
+            class="
+              mt-4
+              text-xl
+              font-bold
+              w-full
+              text-white
+              bg-red-600
+              py-2
+              rounded-xl
+              shadow-lg
+            "
+          >
+            Ler comentários
+          </button>
+        </g-link>
       </div>
     </div>
   </article>
